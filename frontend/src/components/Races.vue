4d87a0c201340c8a460f8ef6011db0e62f196afb
Mattiasr <r.o.n.g.e@hotmail.com>
2019-11-22 22:58:18 +0100

    Added Vuetify-Cards

--- frontend/src/components/Races.vue
+++ frontend/src/components/Races.vue
@@ -1,23 +1,83 @@
 <template>
-  <div class="hello">
+  <div class="races">
 
-        <ul>
-            <li v-for="race in races" :key="race._id" :race="race" @click="$router.push(`/races/${race._id}`)">
-            {{race.name}} <em>info</em></li>
-        </ul>
+        
+
+          <v-card
+    class="mx-auto"
+    max-width="700"
+  >
+
+    <v-toolbar
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Discover</v-toolbar-title> 
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="race in races"
+          :key="race._id"
+          :cols="race.flex"
+        >          
+          <v-card>
+
+            <v-img
+              :src="race.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px" 
+            >
+ <v-img src="@/assets/logo.png"></v-img>
+
+     
+            </v-img>
+       {{ race.name }}
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
   </div>
+  
 </template>
 
 <script>
 export default {
     name : 'races',
+
         beforeMount () {
-        /* Innan sidan laddas, ska den först hämta våra stugor */
+        /* Innan sidan laddas, ska den först hämta datan */
         this.$store.dispatch('getRace');
     },
     computed : {
       races () {
-          /* Hämta alla stugor */
+          /* Hämta alla races */
             return this.$store.state.races;
         }
     }
--- frontend/src/store/mutations.js
+++ frontend/src/store/mutations.js
@@ -6,15 +6,15 @@ export default {
 toggleRejected(state){
   state.rejected = !state.rejected;
 },
-    /* Har du bokat en stuga så lagras den här */
+    /* Har du bokat så lagras det här */
     setBooked(state, eBooked) {
         state.eBooked = eBooked;
     },
-    /* Våra stugor som finns på våran DB samlas här */
+    /* Våra loppen som finns på våran DB samlas här */
     setRaces (state, races) {
         state.races = races;
     },
-    /* När vi tillverkar en ny stuga så sparas det även här */
+
     setRace (state, race) {
         state.race = race;
     }
